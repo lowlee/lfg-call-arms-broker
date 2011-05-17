8171a4c1b3bfc5586a99766f61c9b5b149f337bf
Kevin Ballard <kevin@sb.org>
2011-05-16 23:12:37 -0700

    Don't try to show CtA info when in a party or raid

--- Core.lua
+++ Core.lua
@@ -1,9 +1,15 @@
 local LDB = LibStub("LibDataBroker-1.1")
 local dataobj = LDB:NewDataObject("LFG Call To Arms")
 
+local GROUP_TYPE_NONE = 1
+local GROUP_TYPE_PARTY = 2
+local GROUP_TYPE_RAID = 3
+
 dataobj.type = "data source"
-dataobj.icon = "Interface\\AddOns\\LFGCallToArmsBroker\\Eye"
+dataobj.icon = "Interface\\LFGFrame\\LFG-Eye"
+dataobj.iconCoords = {0.023, 0.102, 0.043, 0.199}
 dataobj.lfginfo = {}
+dataobj.group_type = GROUP_TYPE_NONE
 
 local function calculateTexture(role, size, offset)
 	size = size or 16
@@ -23,88 +29,120 @@ local DPS_TEXTURE = calculateTexture("DAMAGER", 16 , -2)
 LibStub("AceEvent-3.0"):Embed(dataobj)
 LibStub("AceTimer-3.0"):Embed(dataobj)
 
-function dataobj:LFG_LOCK_INFO_RECEIVED()
-	self:UpdateText()
-end
-
-dataobj:RegisterEvent("LFG_LOCK_INFO_RECEIVED")
-
 local function isRandomDungeonDisplayable(id)
 	local name, typeID, minLevel, maxLevel, _, _, _, expansionLevel = GetLFGDungeonInfo(id);
 	local myLevel = UnitLevel("player");
 	return myLevel >= minLevel and myLevel <= maxLevel and EXPANSION_LEVEL >= expansionLevel;
 end
 
+local function currentGroupType()
+	if GetNumRaidMembers() >= 1 then
+		return GROUP_TYPE_RAID
+	elseif GetNumPartyMembers() > 0 then
+		return GROUP_TYPE_PARTY
+	else
+		return GROUP_TYPE_NONE
+	end
+end
+
 function dataobj:UpdateText()
 	table.wipe(self.lfginfo)
-	local hasTank, hasHeal, hasDPS
-	for i=1, GetNumRandomDungeons() do
-		local id, name = GetLFGRandomDungeonInfo(i)
-		if isRandomDungeonDisplayable(id) then
-			local dungeonInfo = {}
-			for i=1, LFG_ROLE_NUM_SHORTAGE_TYPES do
-				local eligible, forTank, forHealer, forDamage, itemCount, money, xp = GetLFGRoleShortageRewards(id, i);
-				if eligible and (itemCount ~= 0 or money ~= 0 or xp ~= 0) then
-					if forTank then
-						hasTank = true
-						dungeonInfo.tank = true
-					end
-					if forHealer then
-						hasHeal = true
-						dungeonInfo.heal = true
-					end
-					if forDamage then
-						hasDPS = true
-						dungeonInfo.dps = true
+	self.group_type = currentGroupType()
+	if self.group_type == GROUP_TYPE_NONE then
+		local hasTank, hasHeal, hasDPS
+		for i=1, GetNumRandomDungeons() do
+			local id, name = GetLFGRandomDungeonInfo(i)
+			if isRandomDungeonDisplayable(id) then
+				local dungeonInfo = {}
+				for i=1, LFG_ROLE_NUM_SHORTAGE_TYPES do
+					local eligible, forTank, forHealer, forDamage, itemCount, money, xp = GetLFGRoleShortageRewards(id, i);
+					if eligible and (itemCount ~= 0 or money ~= 0 or xp ~= 0) then
+						if forTank then
+							hasTank = true
+							dungeonInfo.tank = true
+						end
+						if forHealer then
+							hasHeal = true
+							dungeonInfo.heal = true
+						end
+						if forDamage then
+							hasDPS = true
+							dungeonInfo.dps = true
+						end
 					end
 				end
-			end
-			if next(dungeonInfo) then
-				dungeonInfo.name = name
-				table.insert(self.lfginfo, dungeonInfo)
+				if next(dungeonInfo) then
+					dungeonInfo.name = name
+					table.insert(self.lfginfo, dungeonInfo)
+				end
 			end
 		end
-	end
-	local textures = {}
-	if hasTank then
-		table.insert(textures, TANK_TEXTURE)
-	end
-	if hasHeal then
-		table.insert(textures, HEAL_TEXTURE)
-	end
-	if hasDPS then
-		table.insert(textures, DPS_TEXTURE)
-	end
-	if #textures > 0 then
-		dataobj.text = " "..table.concat(textures, " ")
+		local textures = {}
+		if hasTank then
+			table.insert(textures, TANK_TEXTURE)
+		end
+		if hasHeal then
+			table.insert(textures, HEAL_TEXTURE)
+		end
+		if hasDPS then
+			table.insert(textures, DPS_TEXTURE)
+		end
+		if #textures > 0 then
+			dataobj.text = " "..table.concat(textures, " ")
+		else
+			dataobj.text = " None"
+		end
+	elseif self.group_type == GROUP_TYPE_PARTY then
+		dataobj.text = " In Party"
 	else
-		dataobj.text = " None"
+		dataobj.text = " In Raid"
 	end
+end
+
+function dataobj:GroupMakeupChanged()
+	local groupType = currentGroupType()
+	if (groupType == GROUP_TYPE_NONE) ~= (self.group_type == GROUP_TYPE_NONE) then
+		RequestLFDPlayerLockInfo();
+	end
+end
+
+function dataobj:PARTY_MEMBERS_CHANGED()
+	self:UpdateText()
 end
 
 function dataobj:OnTooltipShow()
 	self:AddLine("LFG Call to Arms")
 	local greyColor = { 0.6, 0.6, 0.6 }
-	local TANK = calculateTexture("TANK", 12)
-	local HEAL = calculateTexture("HEALER", 12)
-	local DPS = calculateTexture("DAMAGER", 12)
-	if #dataobj.lfginfo > 0 then
-		for _,dungeonInfo in ipairs(dataobj.lfginfo) do
-			self:AddLine(dungeonInfo.name, unpack(greyColor))
-			if dungeonInfo.tank then
-				self:AddLine("  " .. TANK .. " Tank")
+	if dataobj.group_type == GROUP_TYPE_NONE then
+		local TANK = calculateTexture("TANK", 12)
+		local HEAL = calculateTexture("HEALER", 12)
+		local DPS = calculateTexture("DAMAGER", 12)
+		if #dataobj.lfginfo > 0 then
+			for _,dungeonInfo in ipairs(dataobj.lfginfo) do
+				self:AddLine(dungeonInfo.name, unpack(greyColor))
+				if dungeonInfo.tank then
+					self:AddLine("  " .. TANK .. " Tank")
+				end
+				if dungeonInfo.heal then
+					self:AddLine("  " .. HEAL .. " Healer")
+				end
+				if dungeonInfo.dps then
+					self:AddLine("  " .. DPS .. " DPS")
+				end
 			end
-			if dungeonInfo.heal then
-				self:AddLine("  " .. HEAL .. " Healer")
-			end
-			if dungeonInfo.dps then
-				self:AddLine("  " .. DPS .. " DPS")
-			end
+		else
+			self:AddLine("No Call to Arms is available.", unpack(greyColor))
 		end
+	elseif dataobj.group_type == GROUP_TYPE_PARTY then
+		self:AddLine("You are in a party.", unpack(greyColor))
 	else
-		self:AddLine("No Call to Arms is available.", unpack(greyColor))
+		self:AddLine("You are in a raid.", unpack(greyColor))
 	end
 end
+
+dataobj:RegisterEvent("LFG_LOCK_INFO_RECEIVED", "UpdateText")
+dataobj:RegisterEvent("PARTY_MEMBERS_CHANGED", "GroupMakeupChanged")
+dataobj:RegisterEvent("RAID_ROSTER_UPDATE", "GroupMakeupChanged")
 
 dataobj:UpdateText()
 dataobj:ScheduleRepeatingTimer(function() RequestLFDPlayerLockInfo() end, 60)
